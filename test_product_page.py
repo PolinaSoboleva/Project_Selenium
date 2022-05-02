09e4d25a7782eacf795df60e9484f77f8f3e7cb1
Polina <lokpolinaa@gmail.com>
2022-05-03 00:51:47 +0300

    2 methods moved to BasePage

--- test_product_page.py
+++ test_product_page.py
@@ -35,3 +35,14 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_be_add_basket_button()
     #page.solve_quiz_and_get_code()
     page.should_be_and_disappear()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
