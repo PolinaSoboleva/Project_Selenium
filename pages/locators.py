9763d264968a1dc597b9fb40287347f5003bfda4
Polina <lokpolinaa@gmail.com>
2022-05-02 22:55:17 +0300

    Add ProductPage test

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGIN_REGISTER = (By.CSS_SELECTOR, "#register_form")
+    
+class ProductPageLocators():
+    ADD_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    BASKET_STRONG_NAMES = (By.CSS_SELECTOR, ".alertinner strong")
+    PRODUCT_NAME=(By.CSS_SELECTOR, "div.product_main h1")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
