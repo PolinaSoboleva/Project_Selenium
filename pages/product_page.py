0843cc207c47fad0b52491f0c6163f1cf6b1c883
Polina <lokpolinaa@gmail.com>
2022-05-03 00:10:27 +0300

    Add negative tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -18,3 +18,10 @@ class ProductPage(BasePage):
             if item_strong.text == product_name:
                 names_equal = True
         assert names_equal, "Names of product isn't equal"
+    
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"
+    def should_be_and_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is not disappeared, but should be"
--- test_product_page.py
+++ test_product_page.py
@@ -11,3 +11,27 @@ def test_guest_can_add_product_to_basket(browser,link1):
     page.solve_quiz_and_get_code()
     page.should_cost_equal()
     page.should_name_equal()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_basket_button()
+    #page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_basket_button()
+    #page.solve_quiz_and_get_code()
+    page.should_be_and_disappear()
